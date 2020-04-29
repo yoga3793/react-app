e5619973c331d95613beb3c022f57c65c7706264
Yogananthan <yogananthan@127.0.0.1>
2020-04-29 10:39:41 +0530

    api changes

--- api/data.js
+++ api/data.js
@@ -1,83 +1,145 @@
 const products = [
-  {
-    id: 01,
-    name: 'Product 01',
-    available_quantity: 5,
-    price: 50,
-    description: 'The New Products description '
-  },
+  { 
+    "_id" : "5ea58374ce1004314ef35f43", 
+    "name" : "Vu Premium 126cm (50 inch)", 
+    "available_quantity" : 100, 
+    "price" : 31999, 
+    "description" : "Show your love for the sport of cricket by bringing home this 4K TV from Vu. Featuring the Cricket Mode and Dolby Vision, this Android TV enhances the contrast and the colour accuracy of the content that you’re watching on it to deliver crystal-clear picture-quality that is nothing short of a visual treat.", 
+    "image" : "https://rukminim1.flixcart.com/image/416/416/k2w6xe80/television/x/h/z/vu-50-oa-original-imafm59f5fjc6a8j.jpeg?q=70", 
+    "specification" : [
+        "Supported Apps:  Hotstar|Youtube", 
+        "Operating System: Android (Google Assistant & Chromecast in-built)", 
+        "Resolution: Ultra HD (4K) 3840 x 2160 Pixels", 
+        "Sound Output: 24 W", 
+        "Refresh Rate: 60 Hz"
+    ], 
+    "category" : [
+        "tv"
+    ]
+},
+{ 
+    "_id" : "5ea58374ce1004314ef35f44", 
+    "name" : "Thomson B9 Pro 102cm (40 inch)", 
+    "available_quantity" : 100, 
+    "price" : 15999, 
+    "description" : "Bring home this Thomson TV that has a 102 cm (40) Full-HD display and enjoy your favourite content. The resolution of this TV panel is 1920x1080. The 20-W speakers deliver a satisfying listening experience. You can enjoy streaming online content and also stream content directly from your smartphone using the screen mirroring feature.", 
+    "image" : "https://rukminim1.flixcart.com/image/416/416/k2w6xe80/television/y/v/m/thomson-65-oath-7000-original-imafm59fksgx2qhz.jpeg?q=70", 
+    "specification" : [
+        "Netflix|Prime Video|Hotstar|Youtube", 
+        "Operating System: Android Based", 
+        "Full HD 1920 x 1080 Pixels", 
+        "20 W Speaker Output", 
+        "60 Hz Refresh Rate", 
+        "3 x HDMI | 2 x USB", 
+        "A+ Samsung Panel", 
+        "My wall interface with 10000000+ hrs of content", 
+        "1 Year Warranty"
+    ], 
+    "category" : [
+        "tv"
+    ]
+},
+{ 
+    "_id" : "5ea58374ce1004314ef35f45", 
+    "name" : "Thomson R9 80cm (32 inch)", 
+    "available_quantity" : 100, 
+    "price" : 7999, 
+    "description" : "Bring home this Thomson TV that has a 102 cm (40) Full-HD display and enjoy your favourite content. The resolution of this TV panel is 1920x1080. The 20-W speakers deliver a satisfying listening experience. You can enjoy streaming online content and also stream content directly from your smartphone using the screen mirroring feature.", 
+    "image" : "https://rukminim1.flixcart.com/image/416/416/k0tw13k0/television/n/h/b/thomson-b9-pro-40m4099-40m4099-pro-original-imafkjazmmrurw7t.jpeg?q=70", 
+    "specification" : [
+        "HD Ready 1366 x 768 Pixels", 
+        "20 W Speaker Output", 
+        "60 Hz Refresh Rate", 
+        "2 x HDMI | 2 x USB", 
+        "Thin Bezel Design", 
+        "1 Year Warranty"
+    ], 
+    "category" : [
+        "tv"
+    ]
+},
+{ 
+    "_id" : "5ea58374ce1004314ef35f46", 
+    "name" : "Thomson 108cm (43 inch)", 
+    "available_quantity" : 100, 
+    "price" : 23999, 
+    "description" : "Watch what you love in the comfort of your house with this LED Smart Android TV from Thomson. It comes with Dolby Audio and DTS technologies for powerful surround sound, easy wireless connectivity, Google Assistant button for fulfilling your everyday needs, and lots more.", 
+    "image" : "https://rukminim1.flixcart.com/image/416/416/k2w6xe80/television/y/v/m/thomson-65-oath-7000-original-imafm59fksgx2qhz.jpeg?q=70", 
+    "specification" : [
+        "Supported Apps: Netflix|Hotstar|Youtube", 
+        "Operating System: Android (Google Assistant & Chromecast in-built)", 
+        "Resolution: Ultra HD (4K) 3840 x 2160 Pixels", 
+        "Sound Output: 30 W", 
+        "Refresh Rate: 60 Hz"
+    ], 
+    "category" : [
+        "tv"
+    ]
+},
+{ 
+    "_id" : "5ea58374ce1004314ef35f47", 
+    "name" : "Mi LED Smart TV 4A PRO", 
+    "available_quantity" : 100, 
+    "price" : 12499, 
+    "description" : "There is no fun in watching your favourite movie or show on a TV where the display quality is poor. Now, boost the fun and watch them all in good and clear-quality on this 80 cm (32) Mi smart TV. Its HD Ready display can accentuate your viewing experience and make it better. You can even access video streaming apps on it and never run out of quality-content to watch and enjoy. It comes with 2 speakers that deliver powerful audio which lets you experience the environment of a cinema hall right in your bedroom or living room.", 
+    "image" : "https://rukminim1.flixcart.com/image/416/416/k4ehnrk0/television/8/4/z/mi-l32m5-an-original-imafm59fhkvg9sr5.jpeg?q=70", 
+    "specification" : [
+        "Supported Apps: Netflix|Prime Video|Hotstar|Youtube", 
+        "Operating System: Android (Google Assistant & Chromecast in-built)", 
+        "Resolution: HD Ready 1366 x 768 Pixels", 
+        "Sound Output: 20 W", 
+        "Refresh Rate: 60 Hz"
+    ], 
+    "category" : [
+        "tv"
+    ]
+}
 
-  {
-    id: 02,
-    name: 'Product 02',
-    available_quantity: 7,
-    price: 50,
-    description: 'The New Products description '
-  },
-
-  {
-    id: 03,
-    name: 'Product 03',
-    available_quantity: 0,
-    price: 50,
-    description: 'The New Products description '
-  },
-
-  {
-    id: 04,
-    name: 'Product 04',
-    available_quantity: 4,
-    price: 50,
-    description: 'The New Products description '
-  },
-  {
-    id: 05,
-    name: 'Product 05',
-    available_quantity: 4,
-    price: 50,
-    description: 'The New Products description '
-  },
-  {
-    id: 06,
-    name: 'Product 06',
-    available_quantity: 4,
-    price: 50,
-    description: 'The New Products description '
-  },
-  {
-    id: 07,
-    name: 'Product 07',
-    available_quantity: 4,
-    price: 50,
-    description: 'The New Products description '
-  },
-  {
-    id: 08,
-    name: 'Product 08',
-    available_quantity: 4,
-    price: 50,
-    description: 'The New Products description '
-  },
-  {
-    id: 09,
-    name: 'Product 09',
-    available_quantity: 4,
-    price: 50,
-    description: 'The New Products description '
-  },
 ];
 
 
 
 const users = [
-    {
-      'email': 'user@gmail.com',
-      'password': 'user'
-    },
-    {
-      'email': 'example',
-      'password': 'qwerty'
-    }
+  { 
+    "_id" : "5ea03c25dca0ec2ace6b3e9a", 
+    "email" : "user1@gmail.com", 
+    "name" : "user1", 
+    "password" : "user1", 
+    "phone" : "+91123456789"
+},
+{ 
+    "_id" : "5ea03c25dca0ec2ace6b3e9b", 
+    "email" : "user2@gmail.com", 
+    "name" : "user2", 
+    "password" : "user2", 
+    "phone" : "+91123456789"
+},
+{ 
+    "_id" : "5ea03c25dca0ec2ace6b3e9c", 
+    "email" : "user3@gmail.com", 
+    "name" : "user3", 
+    "password" : "user3", 
+    "phone" : "+91123456789"
+},
+{ 
+    "_id" : "5ea1ad4dd2102e6eb1d3baeb", 
+    "email" : "email@email.com", 
+    "name" : "req.body.name", 
+    "password" : "req.body.password"
+},
+{ 
+    "_id" : "5ea1ad86d2102e6eb1d3baec", 
+    "email" : "", 
+    "name" : "req.body.name", 
+    "password" : "req.body.password"
+},
+{ 
+    "_id" : "5ea1b3f933d88775eb087807", 
+    "email" : "test@gmail.com", 
+    "name" : "test", 
+    "password" : "test"
+}
+
 ];
 
 module.exports = { 'products': products, users: users }
--- api/server.js
+++ api/server.js
@@ -5,9 +5,11 @@ const app = express();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const data = require('./data');
+// const data = require('./data');
 const middleware = require('./middleware');
-const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const ObjectID = mongodb.ObjectID;
 const url = "mongodb+srv://tetet.@shopping-cart-cluster-dremn.mongodb.net/shopping-cart?retryWrites=true&w=majority";
 
 
@@ -18,21 +20,138 @@ app.use(cors());
 
 // Get Products
 app.get('/api/products', (req, res) => {
-  MongoClient.connect(url, (err, db) => {
-    if (err) throw err;
+    MongoClient.connect(url, (err, db) => {
     var dbo = db.db("shopping-cart");
-    dbo.collection("products").find({}).toArray((err, result) => {
-      if (err) throw err;
-      res.json(result) 
-      console.log(result.name);
+    dbo.collection("products")
+    .find({})
+    .toArray((err, result) => {
       db.close();
+      if (err) {        
+        res.json({"error" : "Database Connection Failed"}) 
+      } else {
+        try {
+          res.json(result);
+        } catch (e) {
+          res.json({"error" : e}) 
+        }
+      }
     });
   });  
   return res;
 });
 
+// Get Product
+
+app.post('/api/product', (req, res) => {
+  
+  MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    var dbo = db.db("shopping-cart");
+    var id = { "_id" : ObjectID(req.body.product_id) }
+    dbo.collection("products").find(id).toArray((err, result) => {
+      db.close();
+      if (err) {        
+        res.json({"error" : "Database Connection Failed"}) 
+      } else {
+        try {
+          res.json(result);
+        } catch (e) {
+          res.json({"error" : e}) 
+        }
+      }
+    });
+  });  
+  return res;
+});
+
+  // Get Cart
+  app.post('/api/get-cart', (req, res) => {
+    let products = [], id = null;
+    let cart = JSON.parse(req.body.cart);
+    if (!cart) return res.json(products);
+
+    var cart_product_ids = Object.keys(cart).map(function(key) {
+      return ObjectID(key);
+    });
+    MongoClient.connect(url, (err, db) => {
+      if (err) throw err;
+      var dbo = db.db("shopping-cart");
+      dbo.collection("products").find({_id: {$in: cart_product_ids}}).toArray((err, result) => {
+        if (err) throw err;        
+        for (var i = 0; i < result.length; i++) {
+          id = result[i]._id.toString();
+          if (cart.hasOwnProperty(id)) {
+            result[i].qty = cart[id]
+            products.push(result[i]);
+          }
+        }
+        res.json(products) 
+        db.close();
+      });
+    });  
+
+    
+    return res;
+
+  });
+
+  // login user
+app.post('/api/login', (req,res) => {
+  let user = [];
+  MongoClient.connect(url, (err, db) =>  {
+    if (err) throw err;
+    var dbo = db.db("shopping-cart");
+    var query = { email: req.body.email , password : req.body.password };
+    dbo.collection("users").find(query).toArray( (err, result) => {
+           
+      if (result.length){
+        // create a token using user name and password vaild for 2 hours
+        let token_payload = {email: result[0].email, password: result[0].password};
+        let token = jwt.sign(token_payload, "jwt_secret_password", { expiresIn: '2h' });
+        let response = { message: 'Token Created, Authentication Successful!', token: token };
+  
+        // return the information including token as JSON
+        res.status(200).json(response);
+  
+    } else {
+        res.status("400").json("Authentication failed. admin not found.");
+    }
+
+    db.close();
+    });
+  return res;
+  
+  });
+
+  
+});
+
+// register user
+
+app.post('/api/register', (req,res) => {
+
+  MongoClient.connect(url, (err, db) =>{
+    if (err) throw err;phone
+    var dbo = db.db("shopping-cart");
+    var userObj = { phone: req.body.phone, email: req.body.email,name: req.body.name, password: req.body.password};
+    dbo.collection("users").insertOne(userObj, (err, result) =>{
+      if(err){  
+        res.status(400).json({message : 'Registration Failed'})  
+      }  
+      else{  
+        res.status(200).json({result: 'user registred'});
+      }       
+      db.close();
+    });
+  });
+  return res;
+});
+
+
+
 // Get Cart
-app.post('/api/get-cart', (req, res) => {
+app.post('/api/get-cart00', (req, res) => {
+  
   MongoClient.connect(url, (err, db) => {
     if (err) throw err;
     var dbo = db.db("shopping-cart");
@@ -123,117 +242,10 @@ app.post('/api/update-cart', (req, res) => {
 });
 
 
-// login user
-app.post('/api/login', (req,res) => {
-  let user = [];
-  MongoClient.connect(url, (err, db) =>  {
-    if (err) throw err;
-    var dbo = db.db("shopping-cart");
-    var query = { email: req.body.email , password : req.body.password };
-    dbo.collection("users").find(query).toArray( (err, result) => {
-      if (err) throw err; 
-     
-      if (result.length){
-        // create a token using user name and password vaild for 2 hours
-        let token_payload = {email: result[0].email, password: result[0].password};
-        let token = jwt.sign(token_payload, "jwt_secret_password", { expiresIn: '2h' });
-        let response = { message: 'Token Created, Authentication Successful!', token: token };
-  
-        // return the information including token as JSON
-        res.status(200).json(response);
-  
-    } else {
-        res.status("409").json("Authentication failed. admin not found.");
-    }
 
-    db.close();
-    });
-  return res;
-  
-  });
-
-  
-});
-
-// register user
-
-app.post('/api/register', (req,res) => {
-
-  MongoClient.connect(url, (err, db) =>{
-    if (err) throw err;
-    var dbo = db.db("shopping-cart");
-    var userObj = { email: req.body.email,name: req.body.name, password: req.body.password};
-    dbo.collection("users").insertOne(userObj, (err, result) =>{
-      if(err){  
-        res.status(400).json(err)  
-      }  
-      else{  
-        res.status(200).json({result: 'user registred'});
-      }       
-      db.close();
-    });
-  });
-  return res;
-});
 
 const PORT = 5000;
 
 app.listen(PORT);
 console.log('api runnging on port ' + PORT + ': ');
 
-
-
-// 'use strict';
-
-// const express = require('express');
-// const app = express();
-// const jwt = require('jsonwebtoken');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const data = require('./data');
-// const middleware = require('./middleware');
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors());
-
-// app.get('/api/products', (req, res) => {
-//   return res.json(data.products);
-// });
-
-// app.post('/api/products', (req, res) => {
-//   let products = [], id = null;
-//   let cart = JSON.parse(req.body.cart);
-//   if (!cart) return res.json(products)
-//   for (var i = 0; i < data.products.length; i++) {
-//     id = data.products[i].id.toString();
-//     if (cart.hasOwnProperty(id)) {
-//       data.products[i].qty = cart[id]
-//       products.push(data.products[i]);
-//     }
-//   }
-//   return res.json(products);
-// });
-
-// app.post('/api/auth', (req,res) => {
-//   let user = data.users.filter((user) => {
-//     return user.name == req.body.name && user.password == req.body.password;
-//   });
-//   if (user.length){
-//       // create a token using user name and password vaild for 2 hours
-//       let token_payload = {name: user[0].name, password: user[0].password};
-//       let token = jwt.sign(token_payload, "jwt_secret_password", { expiresIn: '2h' });
-//       let response = { message: 'Token Created, Authentication Successful!', token: token };
-
-//       // return the information including token as JSON
-//       return res.status(200).json(response);
-
-//   } else {
-//       return res.status("409").json("Authentication failed. admin not found.");
-//   }
-// });
-
-// const PORT = 5000;
-
-// app.listen(PORT);
-// console.log('api runnging on port ' + PORT + ': ');
